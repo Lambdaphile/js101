101b4fff35ca7fc48ee34f7bd72545d9e5e522b2
genesis-algorithms <genesisalgorithms@gmail.com>
2019-08-01 15:01:10 +0300

    refactor: Improve naming and add code explanation comments

--- scripts/app.js
+++ scripts/app.js
@@ -1,72 +1,86 @@
-// Global variables (accessible from everything)
-let data = localStorage.getItem('todolist');
-if (data === null){
-    data = {
-        todolist: []
-    };
-    localStorage.setItem('todolist',JSON.stringify(data));
+// Initalizing our empty global storage object
+let todoList = localStorage.getItem('todoList');
+// Adds an empty array inside our object where
+// we gonna save our todo-list tasks
+if (todoList === null) {
+  todoList = {
+    tasks: []
+  };
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
-save = (data) => {
-    localStorage.setItem('todolist',JSON.stringify(data));
-    render();
+// Gets and returns our localStorage object 'todoList'
+getTodoList = () => {
+  return JSON.parse(localStorage.getItem('todoList'));
 }
 
-remove = (taskid) => {
-    data = getTodoList();
-    data.todolist.splice(taskid,1);
-    save(data);
+save = (todoList) => {
+  localStorage.setItem('todoList', JSON.stringify(todoList));
+  render();
 }
 
-reverse = (value) => {
-    let newVal  = value.split('');
-    newVal.reverse();
-    return newVal.join('-');
+// Removes a task from localStorage object 'todoList'
+remove = (taskId) => {
+  todoList = getTodoList();
+  todoList.tasks.splice(taskId, 1);
+  save(todoList);
+}
+
+// Reverses the task text...
+reverse = (taskText) => {
+  let reversedTaskText = taskText.split('');
+  reversedTaskText.reverse();
+  return reversedTaskText.join('-');
 }
 
 render = () => {
-    data = getTodoList();
-    html = data.todolist.map( (item, i)=>{
-       return '<li>'+item+' <span class="remove" data-taskid="'+i+'">[sil]</span></li>';
-    });
+  todoList = getTodoList();
+  // Gets previously set tasks from localStore
+  // and wraps them with html tags
+  let tasksHTML = todoList.tasks.map((taskText, i) => {
+    return `<li>
+              ${taskText}
+              <span class="remove-task" data-taskId="${i}">
+                [Remove]
+              </span>
+            </li>`;
+  });
 
-    const todolistContainer = document.getElementById('todolist');
-    todolistContainer.innerHTML = html;
+  // Inserts our tasks into the document inside the
+  // our unordered-list #todo-list
+  const todoListHTML = document.getElementById('todo-list');
+  todoListHTML.innerHTML = tasksHTML;
 
-    Array.from(document.getElementsByClassName('remove')).forEach((removeBtn) => {
-        removeBtn.addEventListener('click', (event) => {
-            remove(event.target.getAttribute('data-taskid'));
-        }, false);
-    });
+  // Gets each element with class name 'remove-task'
+  // and adds 'click' listener to them.
+  Array.from(document.getElementsByClassName('remove-task')).forEach((removeBtn) => {
+    removeBtn.addEventListener('click', (event) => {
+      // When 'remove-task' span is clicked, remove the task from
+      // localStorage and document
+      remove(event.target.getAttribute('data-taskId'));
+    }, false);
+  });
 }
 
-getTodoList = () => {
-    return JSON.parse(localStorage.getItem('todolist'));
+addTask = (event) => {
+  // Prevents the form from reloading the page
+  event.preventDefault();
+  
+  todoList = getTodoList();
+  // Gets new task value from todo-lists input
+  const newTask = document.getElementById('todo-input').value;
+
+  // Checks for empty strings and null values
+  if (newTask.trim().length > 0) {
+    // Pushes the new 'task' array into the todoList objects
+    todoList.tasks.push(newTask);
+    // Save the new task into our localStorage
+    save(todoList);
+  }
 }
 
-function doSubmit(event){
-    // Prevents the form from reloading the page
-    event.preventDefault();
-
-    let data = getTodoList();
-    
-    const value = document.getElementById('myinput').value;
-    alert(value);
-    // Seems like this is never used?
-    const result = document.getElementById('result');
-    
-    // Checks for empty strings and null values
-    if (value.trim().length > 0) {
-        // Push the new value into the data objects array
-        // and save the new value into our localStorage
-        data.todolist.push(value);
-        save(data);
-    }
-    // I'll leave this for now, not exaxtly sure what it does.
-    return false;
-}
-
-document.getElementById('form').addEventListener('submit', (event) => {
-    doSubmit(event);
-    render();
+// Gets the todo-list's form and listens for submisions
+document.getElementById('todo-form').addEventListener('submit', (event) => {
+  addTask(event);
+  render();
 }, false);
