2f39fa2c8b449f1a1d0dca17f470b87306a9159d
genesis-algorithms <genesisalgorithms@gmail.com>
2019-08-02 19:10:31 +0300

    THIS COMMIT IT DOES NOT COUNT :D

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,53 @@
+import { default as ToDoList } from './classes/todo-list.js';
+
+let newList = document.querySelector('.new-list');
+
+newList.addEventListener('click', () => {
+  let newTodoList = new ToDoList(prompt('Enter list name'));
+
+  let taskBox = document.createElement('div');
+  taskBox.setAttribute('class', 'task-box');
+
+  let ul = document.createElement('ul');
+  ul.setAttribute('class', 'todo-list');
+
+  document.querySelector('.board').innerHTML = `
+    <p style="margin-left:150px;">${newTodoList.todoListName}</p>
+    <p class="new-list2" style="margin-left:400px;">Add New List</p>
+    <div class="task-box"></div>
+  `;
+  document.querySelector('.task-box').innerHTML = `
+    <ul class="todo-list"></ul>
+  `;
+
+  let addBtn = document.createElement('button');
+  addBtn.setAttribute('class', 'add');
+  addBtn.textContent = 'Add New task  ';
+  document.querySelector('.todo-list').appendChild(addBtn);
+
+  let index = 0;
+  document.querySelector('.add').addEventListener('click', () => {
+    newTodoList.push(prompt('Enter task:'));
+
+    let li = document.createElement('li');
+    
+    li.textContent = newTodoList.todoList.tasks[index];
+    index++;
+    document.querySelector('.todo-list').appendChild(li);
+    let remove = document.createElement('span');
+    remove.setAttribute('class', 'remove-task');
+    remove.setAttribute('data-index', index);
+    remove.textContent = ' [Remove]';
+
+    li.appendChild(remove);
+
+    remove.addEventListener('click', (event) => {
+      console.log(event.target.getAttribute('data-index'));
+      newTodoList.remove('ff');
+    });
+  });
+});
+
 // Initalizing our empty global storage object
 let todoList = localStorage.getItem('todoList');
 // Adds an empty array inside our object where
--- src/scripts/classes/todo-list.js
+++ src/scripts/classes/todo-list.js
@@ -1,7 +1,7 @@
-export default class LocalStorage {
+// eslint-disable-next-line no-unused-vars
+export default class ToDoList {
   constructor(todoListName) {
     this.todoListName = todoListName;
-
     this.todoList = {
       tasks: [],
     };
@@ -15,8 +15,14 @@ export default class LocalStorage {
   }
 
   remove(taskId) {
-    window.localstorage.getItem(this.todoListName);
+    // eslint-disable-next-line no-undef
+    localStorage.getItem(this.todoListName);
     this.todoList.tasks.splice(taskId, 1);
+    this.save();
+  }
+
+  push(newTask) {
+    this.todoList.tasks.push(newTask);
     this.save();
   }
 }
