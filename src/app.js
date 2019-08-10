08f04978ca427ffa19493e02a78f7d9663d2822c
genesis-algorithms <genesisalgorithms@gmail.com>
2019-08-10 18:52:04 +0300

    feat: Make form submision create new todo-list with forms input value

--- src/app.js
+++ src/app.js
@@ -13,9 +13,43 @@ $('.close').click(() => {
 
 form.submit((event) => {
   event.preventDefault();
-  todoList.toggleClass('idle');
-});
 
-$('.composer').click(() => {
-  console.log('yay');
+  const todoListTitle = $('.todo-list-title').val();
+  if (todoListTitle.trim().length > 0) {
+    $('.todo-list.creator').before(`
+      <div class="todo-list">
+        <header class="header">
+          <h2>${todoListTitle}</h2>
+          <div class="extras">
+            <i class="small ellipsis horizontal icon"></i>
+          </div>
+        </header>
+
+        <div class="composer">
+          <div class="activator">
+            <i class="icon plus"></i>
+            <span>Add a card</span>
+          </div>
+        </div>
+      </div>
+    `);
+  }
+
+  $('.composer').click(() => {
+    $('.header').after(`
+      <div class="task">
+        <div class="task-text-wrapper">
+          <textarea class="task-text"
+            placeholder="Enter a title for this task..."></textarea>
+        </div>
+
+        <div class="submit">
+          <button class="ui primary button">Add Card</button>
+          <i class="cancel icon"></i>
+          <i class="ellipsis horizontal icon"></i>
+        </div>
+      </div>
+    `);
+    $('.composer').css('display', 'none');
+  });
 });
